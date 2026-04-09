ef4ecb9ce63998dd70c1d48dbb9f2bec9a435587
alex luu <luu.alex98@gmail.com>
2026-04-08 23:02:50 -0400

    fix: update fee

--- src/config/hyperliquid.ts
+++ src/config/hyperliquid.ts
@@ -3,5 +3,5 @@ import type { BuilderConfig } from "@/lib/hyperliquid";
 export const PROJECT_NAME = "HyperOdd";
 export const DEFAULT_BUILDER_CONFIG: BuilderConfig = {
 	b: "0xe2E7BFE54DDfe5983807C187A57A939C33Eaeb2e", // builder
-	f: 3, // fee rate 0.001%
+	f: 1000, // fee rate 0.1%
 };
